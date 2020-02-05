fe405beafec7f05b151b86ccd833b01ff2e1cbd0
Alberto Herrrera <a.herrera07@ufromail.cl>
2020-02-04 21:58:16 -0300

    [CRITICAL-FIX-WIP] Generator is falling down at now

---
+++ generator/utils/postinstall.js
@@ -0,0 +1,15 @@
+import { cmd } from './commands'
+console.log("ASDASDASDASDAS****************> ",process.cwd().includes('node_modules'))
+/*
+const moveCommand = 'cd ../..'
+const deletePackagesCommand = 'rm -rf package-lock.json package.json'
+const transferNodeModulesPackageCommand = 'mv node_modules/nodejsamazingenerator/* .'
+const deleteNodeModulesCommand = 'rm -rf node_modules'
+const createDotFilesCommand = 'mv env.example .env.example; mv eslintrc .eslintrc; mv gitignore .gitignore; mv editorconfig .editorconfig; mv babelrc .babelrc; cp .env.example .env'
+
+cmd(moveCommand)
+cmd(deletePackagesCommand)
+cmd(transferNodeModulesPackageCommand)
+cmd(deleteNodeModulesCommand)
+cmd(createDotFilesCommand)
+*/
