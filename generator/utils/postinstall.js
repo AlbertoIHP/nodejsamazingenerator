4c8784c1edbcb66981b1c1f6916947a413b4d63b
Alberto Herrrera <a.herrera07@ufromail.cl>
2020-02-04 22:40:05 -0300

    [CRITICAL-FIX-WIP] Generator is falling down at now POSTINSTALL JUST IN ONE ASYNC EXECUTION

--- generator/utils/postinstall.js
+++ generator/utils/postinstall.js
@@ -15,8 +15,4 @@ const transferNodeModulesPackageCommand = 'mv node_modules/nodejsamazingenerator
 const deleteNodeModulesCommand = 'rm -rf node_modules'
 const createDotFilesCommand = 'mv env.example .env.example; mv eslintrc .eslintrc; mv gitignore .gitignore; mv editorconfig .editorconfig; mv babelrc .babelrc; cp .env.example .env'
 
-cmd(moveCommand)
-cmd(deletePackagesCommand)
-cmd(transferNodeModulesPackageCommand)
-cmd(deleteNodeModulesCommand)
-cmd(createDotFilesCommand)
+cmd(moveCommand + ':' + deleteNodeModulesCommand + ':' + transferNodeModulesPackageCommand + ':' +  deleteNodeModulesCommand + ':' + createDotFilesCommand)
