8bf3ef0bff9e3b340b5b66c37c4019631e0680d6
Alberto Herrrera <a.herrera07@ufromail.cl>
2020-02-04 22:29:54 -0300

    [CRITICAL-FIX-WIP] Generator is falling down at now

--- generator/utils/postinstall_es5.js
+++ generator/utils/postinstall_es5.js
@@ -1,5 +1,5 @@
 const execSync = require('child_process').execSync
-let postinstallScript = "node ../../node_modules/.bin/babel-node postinstall.js"
+let postinstallScript = "node ../../node_modules/.bin/babel-node generator/utils/postinstall.js"
 
 console.log("ASDASDASDASDAS****************> ",process.cwd().includes('node_modules'))
 
