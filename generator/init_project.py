0639384bccc58139f46c93ed7cc10b6cbea8053f
Alberto Ignacio Herrera Poza <a.herrera07@ufromail.cl>
2020-02-06 10:45:45 -0300

    [WIP] Adding logic to init project with installation of amazingenerator

--- generator/init_project.py
+++ generator/init_project.py
@@ -1,5 +1,6 @@
 import sys, os, struct
 import string
+import base64
 ##
 ## Some fancy stuff for generator :) AHP
 ##
@@ -20,98 +21,105 @@ envs = []
 projectName = raw_input("     > Which is your project name (Enter for default: myproject): ")
 if(projectName == ''):
     projectName = 'myproject'
-envs.append(projectName)
+envs.append('PROJECT_NAME=' + projectName)
 
 ##DB host
 dbHost = raw_input("     > Please enter your DB Host (Enter for default: localhost): ")
 if(dbHost == ''):
     dbHost = 'localhost'
-envs.append(dbHost)
+envs.append('DB_HOST=' + dbHost)
 
 ##DB username
 dbUsername = raw_input("     > Please enter your DB username(Enter for default: postgres): ")
 if(dbUsername == ''):
     dbUsername = 'postgres'
-envs.append(dbUsername)
+envs.append('DB_USER=' + dbUsername)
 
 ##DB password
 dbPassword = raw_input("     > Please enter your DB password(Enter for default: none): ")
 if(dbPassword == ''):
     dbPassword = ''
-envs.append(dbPassword)
+envs.append('DB_PASSWORD=' + dbPassword)
 
 ##DB port
 dbPort = raw_input("     > Please enter your DB port(Enter for default: 5432): ")
 if(dbPort == ''):
     dbPort = '5432'
-envs.append(dbPort)
+envs.append('DB_PORT=' + dbPort)
 
 ##DB dev name
 dbDevName = raw_input("     > Please enter your DB DEV database name (Enter for default: " + projectName +"_dev):")
 if(dbDevName == ''):
     dbDevName = projectName + "_dev"
-envs.append(dbDevName)
+envs.append('DB_NAME_DEV=' + dbDevName)
 
 
 ##DB test name
 dbTestName = raw_input("     > Please enter your DB TEST database name (Enter for default: " + projectName +"_test): ")
 if(dbTestName == ''):
     dbTestName = projectName + "_test"
-envs.append(dbTestName)
+envs.append('DB_NAME_TEST=' + dbTestName)
 
 
 ##DB prod name
 dbProdName = raw_input("     > Please enter your DB PRODUCTION database name (Enter for default: " + projectName +"_prod): ")
 if(dbProdName == ''):
     dbProdName = projectName + "_prod"
-envs.append(dbProdName)
+envs.append('DB_NAME_PROD=' + dbProdName)
 
 
 ##Server IP
 serverIp = raw_input("     > Please enter your server IP (Enter for default: localhost): ")
 if(serverIp == ''):
     serverIp = 'localhost'
-envs.append(serverIp)
+envs.append('SERVER_IP=' + serverIp)
 
 ##Server port
 serverPort = raw_input("     > Please enter your server PORT (Enter for default: 3030): ")
 if(serverPort == ''):
     serverPort = '3030'
-envs.append(serverPort)
+envs.append('SERVER_PORT=' + serverPort)
 
 
 ##Server default email
 serverDefaultEmail = raw_input("     > Please enter your server default email (Enter for default:  no-reply@"+projectName+".com): ")
 if(serverDefaultEmail == ''):
     serverDefaultEmail = "no-reply@" + projectName +".com): "
-envs.append(serverDefaultEmail)
+envs.append('DEFAULT_EMAIL_ALIAS=' + serverDefaultEmail)
 
 ##Server SENDGRID key 
 serverSendgridKey = raw_input("     > Please enter your Sendgrid API Secret  (Enter for default: none")
 if(serverSendgridKey == ''):
     serverSendgridKey = ''
-envs.append(serverSendgridKey)
+envs.append('SENDGRID_KEY=' + serverSendgridKey)
 
 ##Server Logging
 serverLog = raw_input("     >  Want to log server(Enter for default: true")
 if(serverLog == ''):
     serverLog = 'true'
-envs.append(serverLog)
+envs.append('DB_USER=' + serverLog)
 
 ##Server JWT Secret
-serverJwt = raw_input("     >  Which is your JWT key (Enter for default: jwtsecret")
+serverJwt = raw_input("     >  Which is your JWT key this will be encode to base64 string (Enter for default: jwtsecret")
 if(serverJwt == ''):
     serverJwt = 'jwtsecret'
-envs.append(serverJwt)
+envs.append('JWT_SECRET=' + serverJwt)
 
 ##Server Master Secret
-serverMasterSecret = raw_input("     >  Which is your Master key (Enter for default: mymasterkey")
+serverMasterSecret = raw_input("     >  Which is your Master key this will be encode to base64 string (Enter for default: mymasterkey")
 if(serverMasterSecret == ''):
     serverMasterSecret = 'mymasterkey'
-envs.append(serverMasterSecret)
+
+serverMasterSecret = serverMasterSecret.encode('ascii')
+serverMasterSecret = base64.b64encode(serverMasterSecret)
+
+envs.append('MASTER_KEY=' + serverMasterSecret)
+
+print "".join(envs)
+
 
 print "Getting from CLI vars to set .env file :) on devw"
-print envs
+
 
 print WARNING + '  *******       ***********************          *******'
 print OKGREEN + ' **********       ***********************          *******'
